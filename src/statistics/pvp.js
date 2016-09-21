38c01b655ae4cd6078058bc3ee98e47aa1f8ef29
queicherius <queicherius@gmail.com>
2016-09-21 20:48:39 +0200

    Add a bunch of pvp statistics

--- src/statistics/pvp.js
+++ src/statistics/pvp.js
@@ -2,22 +2,54 @@ export default function (accountData) {
   if (!accountData.pvp) {
     return {
       pvpGameCount: null,
+      pvpGameCountRanked: null,
       pvpWinCount: null,
+      pvpWinCountRanked: null,
       pvpRank: null,
-      pvpWinRate: null
+      pvpWinRate: null,
+      pvpWinRate50: null,
+      pvpWinRate250: null,
+      pvpWinRate500: null,
+      pvpWinRate1000: null
     }
   }
 
   // Calculate the real pvp rank
   const pvpRank = accountData.pvp.pvp_rank + accountData.pvp.pvp_rank_rollovers
 
-  // Count the games and wins
-  const aggregate = accountData.pvp.aggregate
-  const pvpGameCount = aggregate.wins + aggregate.losses + aggregate.desertions +
-    aggregate.byes + aggregate.forfeits
-  const pvpWinCount = aggregate.wins + aggregate.byes
-
-  // Calculate the winrate
+  // Count the games and wins for all games
+  const pvpGameCount = getGames(accountData.pvp.aggregate)
+  const pvpWinCount = getWins(accountData.pvp.aggregate)
   const pvpWinRate = pvpGameCount > 0 ? (pvpWinCount / pvpGameCount) * 100 : null
-  return {pvpGameCount, pvpWinCount, pvpRank, pvpWinRate}
+  const pvpWinRate50 = pvpGameCount > 50 ? (pvpWinCount / pvpGameCount) * 100 : null
+  const pvpWinRate250 = pvpGameCount > 250 ? (pvpWinCount / pvpGameCount) * 100 : null
+  const pvpWinRate500 = pvpGameCount > 500 ? (pvpWinCount / pvpGameCount) * 100 : null
+  const pvpWinRate1000 = pvpGameCount > 1000 ? (pvpWinCount / pvpGameCount) * 100 : null
+
+  // Count the games and wins for ranked games
+  const l = accountData.pvp.ladders
+  const pvpGameCountRanked = getGames(l.ranked) + getGames(l.soloarenarated) + getGames(l.teamarenarated)
+  const pvpWinCountRanked = getWins(l.ranked) + getWins(l.soloarenarated) + getWins(l.teamarenarated)
+
+  // Return everything!
+  return {
+    pvpGameCount,
+    pvpGameCountRanked,
+    pvpWinCount,
+    pvpWinCountRanked,
+    pvpRank,
+    pvpWinRate,
+    pvpWinRate50,
+    pvpWinRate250,
+    pvpWinRate500,
+    pvpWinRate1000
+  }
+}
+
+function getWins (aggregate) {
+  return aggregate.wins + aggregate.byes
+}
+
+function getGames (aggregate) {
+  return aggregate.wins + aggregate.losses + aggregate.desertions + aggregate.byes + aggregate.forfeits
 }
--- tests/pvp.spec.js
+++ tests/pvp.spec.js
@@ -3,9 +3,15 @@ const expect = require('chai').expect
 import pvpStatistics from '../src/statistics/pvp'
 const defaultValues = {
   pvpGameCount: null,
+  pvpGameCountRanked: null,
   pvpWinCount: null,
+  pvpWinCountRanked: null,
   pvpRank: null,
-  pvpWinRate: null
+  pvpWinRate: null,
+  pvpWinRate50: null,
+  pvpWinRate250: null,
+  pvpWinRate500: null,
+  pvpWinRate1000: null
 }
 
 describe('statistics > pvp', () => {
@@ -22,14 +28,57 @@ describe('statistics > pvp', () => {
         desertions: 0,
         byes: 3,
         forfeits: 3
+      },
+      ladders: {
+        none: {
+          wins: 0,
+          losses: 0,
+          desertions: 0,
+          byes: 0,
+          forfeits: 0
+        },
+        ranked: {
+          wins: 345,
+          losses: 329,
+          desertions: 0,
+          byes: 2,
+          forfeits: 2
+        },
+        soloarenarated: {
+          wins: 0,
+          losses: 0,
+          desertions: 0,
+          byes: 0,
+          forfeits: 0
+        },
+        teamarenarated: {
+          wins: 292,
+          losses: 268,
+          desertions: 0,
+          byes: 0,
+          forfeits: 0
+        },
+        unranked: {
+          wins: 17,
+          losses: 21,
+          desertions: 0,
+          byes: 1,
+          forfeits: 0
+        }
       }
     }
 
     const output = {
       pvpGameCount: 1360,
+      pvpGameCountRanked: 1238,
       pvpRank: 145,
       pvpWinCount: 714,
-      pvpWinRate: 52.5
+      pvpWinCountRanked: 639,
+      pvpWinRate: 52.5,
+      pvpWinRate50: 52.5,
+      pvpWinRate250: 52.5,
+      pvpWinRate500: 52.5,
+      pvpWinRate1000: 52.5
     }
 
     expect(pvpStatistics({pvp: input})).to.deep.equal(output)
@@ -48,14 +97,57 @@ describe('statistics > pvp', () => {
         desertions: 0,
         byes: 0,
         forfeits: 0
+      },
+      ladders: {
+        none: {
+          wins: 0,
+          losses: 0,
+          desertions: 0,
+          byes: 0,
+          forfeits: 0
+        },
+        ranked: {
+          wins: 0,
+          losses: 0,
+          desertions: 0,
+          byes: 0,
+          forfeits: 0
+        },
+        soloarenarated: {
+          wins: 0,
+          losses: 0,
+          desertions: 0,
+          byes: 0,
+          forfeits: 0
+        },
+        teamarenarated: {
+          wins: 0,
+          losses: 0,
+          desertions: 0,
+          byes: 0,
+          forfeits: 0
+        },
+        unranked: {
+          wins: 0,
+          losses: 0,
+          desertions: 0,
+          byes: 0,
+          forfeits: 0
+        }
       }
     }
 
     const output = {
       pvpGameCount: 0,
+      pvpGameCountRanked: 0,
       pvpRank: 145,
       pvpWinCount: 0,
-      pvpWinRate: null
+      pvpWinCountRanked: 0,
+      pvpWinRate: null,
+      pvpWinRate50: null,
+      pvpWinRate250: null,
+      pvpWinRate500: null,
+      pvpWinRate1000: null
     }
 
     expect(pvpStatistics({pvp: input})).to.deep.equal(output)
