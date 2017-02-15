cd6019a13b7af21bc6a2f56f93c5834672618ad5
queicherius <queicherius@gmail.com>
2017-02-15 20:15:50 +0100

    Add "Endless Repair Canister" to the permanent contracts
    References gw2efficiency/issues#668

--- src/statistics/items.js
+++ src/statistics/items.js
@@ -48,7 +48,7 @@ export default function (accountData) {
     tomesOfKnowledge: countItems(items, [43741, 43766]),
     permanentContracts: countItems(items, [
       35984, 38507, 35985, 35986, 49501,
-      35978, 35977, 35976, 38506
+      35978, 35977, 35976, 38506, 78217, 78455
     ]),
     freshWinterberries: countItems(items, 79899),
     wintersHeartInfusions: countItems(items, [
