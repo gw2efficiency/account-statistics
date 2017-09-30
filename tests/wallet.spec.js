b4e690bbd65954b7407cf8505a54ac34f2a3b3e4
queicherius <queicherius@gmail.com>
2017-09-30 23:24:59 +0100

    :sparkles: Add new PoF currencies

--- tests/wallet.spec.js
+++ tests/wallet.spec.js
@@ -34,44 +34,53 @@ describe('statistics > wallet', () => {
       flameLegionCharrCarvings: null,
       symbolsOfKoda: null,
       knowledgeCrystals: null,
-      shardsOfZhaitan: null
+      shardsOfZhaitan: null,
+      ascendedShardsOfGlory: null,
+      tradeContract: null,
+      elegyMosaic: null,
+      testimonyOfHeroics: null
     })
   })
 
   it('can calculate all wallet values', () => {
     const wallet = [
-      {id: 1, value: 17551485},
-      {id: 2, value: 207320},
-      {id: 3, value: 12},
-      {id: 5, value: 84},
-      {id: 6, value: 35},
-      {id: 7, value: 1168},
-      {id: 9, value: 295},
-      {id: 10, value: 45},
-      {id: 11, value: 173},
-      {id: 12, value: 270},
-      {id: 13, value: 123},
-      {id: 14, value: 253},
-      {id: 15, value: 1688},
-      {id: 16, value: 127},
-      {id: 18, value: 145},
-      {id: 19, value: 24},
-      {id: 20, value: 5},
-      {id: 22, value: 139},
-      {id: 23, value: 2070},
-      {id: 24, value: 48},
-      {id: 25, value: 303},
-      {id: 26, value: 50},
-      {id: 27, value: 1383},
-      {id: 28, value: 197},
-      {id: 29, value: 27},
-      {id: 30, value: 55},
-      {id: 31, value: 112},
-      {id: 32, value: 70}
+      {id: 1, value: 17551485}, // Coin
+      {id: 2, value: 207320}, // Karma
+      {id: 3, value: 12}, // Laurel
+      {id: 4, value: 499}, // Gems
+      {id: 5, value: 84}, // Ascalonian Tear
+      {id: 6, value: 35}, // Shard of Zhaitan
+      {id: 7, value: 1168}, // Fractal Relic
+      {id: 9, value: 295}, // Seal of Beetletun
+      {id: 10, value: 45}, // Manifesto of the Moletariate
+      {id: 11, value: 173}, // Deadly Bloom
+      {id: 12, value: 270}, // Symbol of Koda
+      {id: 13, value: 123}, // Flame Legion Charr Carving
+      {id: 14, value: 253}, // Knowledge Crystal
+      {id: 15, value: 1688}, // Badge of Honor
+      {id: 16, value: 127}, // Guild Commendation
+      {id: 18, value: 145}, // Transmutation Charge
+      {id: 19, value: 24}, // Airship Part
+      {id: 20, value: 5}, // Ley Line Crystal
+      {id: 22, value: 139}, // Lump of Aurillium
+      {id: 23, value: 2070}, // Spirit Shard
+      {id: 24, value: 48}, // Pristine Fractal Relic
+      {id: 25, value: 303}, // Geode
+      {id: 26, value: 50}, // WvW Skirmish Claim Ticket
+      {id: 27, value: 1383}, // Bandit Crest
+      {id: 28, value: 197}, // Magnetite Shard
+      {id: 29, value: 27}, // Provisioner Token
+      {id: 30, value: 55}, // PvP League Ticket
+      {id: 31, value: 112}, // Proof of Heroics
+      {id: 32, value: 70}, // Unbound Magic
+      {id: 33, value: 45}, // Ascended Shards of Glory
+      {id: 34, value: 465}, // Trade Contract
+      {id: 35, value: 98}, // Elegy Mosaic
+      {id: 36, value: 645} // Testimony of Heroics
     ]
 
     expect(walletStatistics({wallet})).to.deep.equal({
-      gems: 0,
+      gems: 499,
       gold: 17551485,
       karma: 207320,
       spiritShards: 2070,
@@ -100,7 +109,11 @@ describe('statistics > wallet', () => {
       symbolsOfKoda: 270,
       knowledgeCrystals: 253,
       shardsOfZhaitan: 35,
-      dungeonTokenCount: 1278
+      dungeonTokenCount: 1278,
+      ascendedShardsOfGlory: 45,
+      tradeContract: 465,
+      elegyMosaic: 98,
+      testimonyOfHeroics: 645
     })
   })
 })
