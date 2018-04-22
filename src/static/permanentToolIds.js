a5182930d886d610ccbc65cbfe450c745f135352
queicherius <queicherius@gmail.com>
2018-04-22 18:38:20 +0100

    :sparkles: Update unlimited gathering tools

--- src/static/permanentToolIds.js
+++ src/static/permanentToolIds.js
@@ -3,47 +3,47 @@ export default [
   67030, // Fused Molten Logging Axe
   41807, // Molten Alliance Mining Pick
   48933, // Molten Alliance Mining Pick
-
   67063, // Tireless Harvesting Minion
   67029, // Tireless Logging Minion
-  43527, // Bone Pick
   48932, // Bone Pick
-
+  43527, // Bone Pick
   67395, // Reap-r-Tron
   67394, // Log-r-Tron
   67396, // Mine-r-Tron
-
   49308, // Thresher-Sickle 5000
-  48931, // Chop-It-All Logging Axe
   42931, // Chop-It-All Logging Axe
+  48931, // Chop-It-All Logging Axe
+  87422, // Watchwork Mining Pick
   47897, // Watchwork Mining Pick
-
   69478, // Mad Scientist's Harvesting Tool
   68799, // Mad Scientist's Logging Tool
   68905, // Mad Scientist's Mining Tool
-
   70044, // Butterfly Harvesting Flute
   69921, // Swarm Logging Flute
   69958, // Firefly Mining Flute
-
   79721, // Cosmic Harvesting Tool
   79472, // Cosmic Logging Tool
   78731, // Cosmic Mining Tool
-
   78996, // Unbreakable Harvesting Sickle
   78806, // Unbreakable Logging Axe
   78812, // Unbreakable Mining Pick
-
+  80987, // Unbound Magic Harvesting Blast
+  87396, // Unbound Magic Harvesting Blast
+  87420, // Unbound Magic Logging Pulse
+  80979, // Unbound Magic Logging Pulse
+  87443, // Unbound Magic Mining Beam
+  80977, // Unbound Magic Mining Beam
   78909, // Glitter Bomb Harvesting Tool
-  48955, // Dreamcleaver Logging Axe
   48930, // Consortium Harvesting Sickle
   42594, // Consortium Harvesting Sickle
-  48825, // Frost Wasp Logging Tool
   48934, // Jack-in-the-Box Scythe
   44876, // Jack-in-the-Box Scythe
-  78724, // Focused Solar Logging Tool
   80067, // Celestial Rooster Harvesting Tool
-  80979, // Unbound-Magic-Logging-Pulse
-  80987, // Unbound-Magic-Harvesting-Blast
-  80977 // Unbound-Magic-Mining-Beam
+  86943, // Lucky Dog Harvesting Tool
+  87397, // Lucky Dog Harvesting Tool
+  48955, // Dreamcleaver Logging Axe
+  48825, // Frost Wasp Logging Tool
+  78724, // Focused Solar Logging Tool
+  80966, // Super Adventure Logging Bear
+  87425 // Shifting Sand Mining Pick
 ]
