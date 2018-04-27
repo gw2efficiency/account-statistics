521d290457577827b6be9cd8e457ee9267efbb03
queicherius <queicherius@gmail.com>
2018-04-27 22:40:55 +0100

    :card_index: Update ids of champion bags

--- src/static/championBagIds.js
+++ src/static/championBagIds.js
@@ -1,94 +1,432 @@
 export default [
-  44147, // Beaded Pouch
-  44133, // Box of Lab Equipment
-  44199, // Branded Strongbox
-  44086, // Deluxe Gear Box
-  44108, // Deluxe Gear Box
-  44113, // Deluxe Gear Box
-  44118, // Deluxe Gear Box
-  44123, // Deluxe Gear Box
-  44128, // Deluxe Gear Box
-  44138, // Deluxe Gear Box
-  44143, // Deluxe Gear Box
-  44471, // Deluxe Gear Box
-  44207, // Elaborate Fur Pouch
-  79224, // Elaborate Fur Pouch
-  44209, // Elaborate Ritualist Bag
-  44241, // Elaborate Ritualist Bag
-  44246, // Elaborate Ritualist Bag
-  44205, // Embroidered Belt Pouch
-  44266, // Embroidered Belt Pouch
-  44196, // Embroidered Coin Purse
-  44251, // Embroidered Coin Purse
-  44197, // Embroidered Saddlebag
-  44256, // Embroidered Saddlebag
-  44261, // Embroidered Saddlebag
-  76830, // Feather-Lined Pouch
+  44077, // Fallen Adventurer's Backpack
+  44078, // Fallen Adventurer's Backpack
+  44079, // Fallen Adventurer's Backpack
+  44080, // Fallen Adventurer's Backpack
   44081, // Fallen Adventurer's Backpack
-  44271, // Fallen Adventurer's Backpack
-  44276, // Fallen Adventurer's Backpack
-  44281, // Fallen Adventurer's Backpack
-  44286, // Fallen Adventurer's Backpack
-  44291, // Fallen Adventurer's Backpack
-  44296, // Fallen Adventurer's Backpack
-  44301, // Fallen Adventurer's Backpack
-  44306, // Fallen Adventurer's Backpack
-  44311, // Fallen Adventurer's Backpack
-  44316, // Fallen Adventurer's Backpack
-  44321, // Fallen Adventurer's Backpack
-  44326, // Fallen Adventurer's Backpack
-  44331, // Fallen Adventurer's Backpack
-  44336, // Fallen Adventurer's Backpack
-  44341, // Fallen Adventurer's Backpack
-  44346, // Fallen Adventurer's Backpack
-  44351, // Fallen Adventurer's Backpack
-  44356, // Fallen Adventurer's Backpack
-  44361, // Fallen Adventurer's Backpack
-  44366, // Fallen Adventurer's Backpack
-  44371, // Fallen Adventurer's Backpack
-  44376, // Fallen Adventurer's Backpack
-  44381, // Fallen Adventurer's Backpack
-  44386, // Fallen Adventurer's Backpack
-  44391, // Fallen Adventurer's Backpack
-  44396, // Fallen Adventurer's Backpack
+  44082, // Deluxe Gear Box
+  44083, // Deluxe Gear Box
+  44084, // Deluxe Gear Box
+  44085, // Deluxe Gear Box
+  44086, // Deluxe Gear Box
+  44087, // Travelers Sack
+  44088, // Elaborate Ritualist Bag
+  44089, // Embroidered Coin Purse
+  44090, // Embroidered Saddlebag
+  44091, // Branded Strongbox
+  44093, // Gilded Strongbox
+  44094, // Oversized Rucksack
+  44095, // Singed Ritualist Bag
+  44096, // Tribal Satchel
+  44097, // Embroidered Belt Pouch
+  44098, // Utility Backpack
+  44099, // Beaded Pouch
+  44100, // Elaborate Fur Pouch
+  44101, // Shell Pouch
+  44102, // Slime-Covered Strongbox
+  44103, // Gilded Coffer
+  44104, // Deluxe Gear Box
+  44105, // Deluxe Gear Box
+  44106, // Deluxe Gear Box
+  44107, // Deluxe Gear Box
+  44108, // Deluxe Gear Box
+  44109, // Deluxe Gear Box
+  44110, // Deluxe Gear Box
+  44111, // Deluxe Gear Box
+  44112, // Deluxe Gear Box
+  44113, // Deluxe Gear Box
+  44114, // Deluxe Gear Box
+  44115, // Deluxe Gear Box
+  44116, // Deluxe Gear Box
+  44117, // Deluxe Gear Box
+  44118, // Deluxe Gear Box
+  44119, // Deluxe Gear Box
+  44120, // Deluxe Gear Box
+  44121, // Deluxe Gear Box
+  44122, // Deluxe Gear Box
+  44123, // Deluxe Gear Box
+  44124, // Deluxe Gear Box
+  44125, // Deluxe Gear Box
+  44126, // Deluxe Gear Box
+  44127, // Deluxe Gear Box
+  44128, // Deluxe Gear Box
+  44129, // Box of Lab Equipment
+  44130, // Box of Lab Equipment
+  44131, // Box of Lab Equipment
+  44132, // Box of Lab Equipment
+  44133, // Box of Lab Equipment
+  44134, // Deluxe Gear Box
+  44135, // Deluxe Gear Box
+  44136, // Deluxe Gear Box
+  44137, // Deluxe Gear Box
+  44138, // Deluxe Gear Box
+  44139, // Deluxe Gear Box
+  44140, // Deluxe Gear Box
+  44141, // Deluxe Gear Box
+  44142, // Deluxe Gear Box
+  44143, // Deluxe Gear Box
+  44144, // Beaded Pouch
+  44145, // Beaded Pouch
+  44146, // Beaded Pouch
+  44147, // Beaded Pouch
+  44148, // Embroidered Coin Purse
+  44149, // Embroidered Saddlebag
+  44151, // Branded Strongbox
+  44152, // Gilded Coffer
+  44153, // Gilded Strongbox
+  44154, // Oversized Rucksack
+  44155, // Singed Ritualist Bag
+  44156, // Tribal Satchel
+  44157, // Embroidered Belt Pouch
+  44158, // Utility Backpack
+  44159, // Elaborate Fur Pouch
+  44160, // Shell Pouch
+  44161, // Elaborate Ritualist Bag
+  44162, // Slime-Covered Strongbox
+  44163, // Travelers Sack
+  44164, // Embroidered Coin Purse
+  44165, // Embroidered Saddlebag
+  44167, // Branded Strongbox
+  44168, // Gilded Coffer
+  44169, // Gilded Strongbox
+  44170, // Oversized Rucksack
+  44171, // Singed Ritualist Bag
+  44172, // Tribal Satchel
+  44173, // Embroidered Belt Pouch
+  44174, // Utility Backpack
+  44175, // Elaborate Fur Pouch
+  44176, // Shell Pouch
+  44177, // Elaborate Ritualist Bag
+  44178, // Slime-Covered Strongbox
+  44179, // Travelers Sack
+  44180, // Embroidered Coin Purse
+  44181, // Embroidered Saddlebag
+  44183, // Branded Strongbox
+  44184, // Gilded Coffer
+  44185, // Gilded Strongbox
+  44186, // Oversized Rucksack
+  44187, // Singed Ritualist Bag
+  44188, // Tribal Satchel
+  44189, // Embroidered Belt Pouch
+  44190, // Utility Backpack
+  44191, // Elaborate Fur Pouch
+  44192, // Shell Pouch
+  44193, // Elaborate Ritualist Bag
+  44194, // Slime-Covered Strongbox
+  44195, // Travelers Sack
+  44196, // Embroidered Coin Purse
+  44197, // Embroidered Saddlebag
+  44198, // Heirloom Seed Pouch
+  44199, // Branded Strongbox
   44200, // Gilded Coffer
   44201, // Gilded Strongbox
-  44421, // Gilded Strongbox
-  44426, // Gilded Strongbox
-  44431, // Gilded Strongbox
-  44436, // Gilded Strongbox
-  44441, // Gilded Strongbox
-  44446, // Gilded Strongbox
-  79144, // Gilded Strongbox
-  79215, // Gilded Strongbox
-  44198, // Heirloom Seed Pouch
-  44221, // Heirloom Seed Pouch
-  44231, // Heirloom Seed Pouch
-  44236, // Heirloom Seed Pouch
-  49039, // Heirloom Seed Pouch
-  44451, // Icy Strongbox
-  44456, // Large Candy Bag
-  73716, // Noxious Seed Pod
   44202, // Oversized Rucksack
-  44401, // Oversized Rucksack
-  44406, // Oversized Rucksack
-  44411, // Oversized Rucksack
-  44416, // Oversized Rucksack
-  44216, // Patchwork Knapsack
-  44208, // Shell Pouch
-  44461, // Shell Pouch
-  44466, // Shell Pouch
-  49038, // Shell Pouch
   44203, // Singed Ritualist Bag
-  44210, // Slime-Covered Strongbox
-  44226, // Thorny Seed Pouch
-  44211, // Travelers Sack
   44204, // Tribal Satchel
+  44205, // Embroidered Belt Pouch
   44206, // Utility Backpack
+  44207, // Elaborate Fur Pouch
+  44208, // Shell Pouch
+  44209, // Elaborate Ritualist Bag
+  44210, // Slime-Covered Strongbox
+  44211, // Travelers Sack
+  44212, // Patchwork Knapsack
+  44213, // Patchwork Knapsack
+  44214, // Patchwork Knapsack
+  44215, // Patchwork Knapsack
+  44216, // Patchwork Knapsack
+  44221, // Heirloom Seed Pouch
+  44222, // Thorny Seed Pouch
+  44223, // Thorny Seed Pouch
+  44224, // Thorny Seed Pouch
+  44225, // Thorny Seed Pouch
+  44226, // Thorny Seed Pouch
+  44231, // Heirloom Seed Pouch
+  44232, // Heirloom Seed Pouch
+  44233, // Heirloom Seed Pouch
+  44234, // Heirloom Seed Pouch
+  44235, // Heirloom Seed Pouch
+  44236, // Heirloom Seed Pouch
+  44237, // Elaborate Ritualist Bag
+  44238, // Elaborate Ritualist Bag
+  44239, // Elaborate Ritualist Bag
+  44240, // Elaborate Ritualist Bag
+  44241, // Elaborate Ritualist Bag
+  44242, // Elaborate Ritualist Bag
+  44243, // Elaborate Ritualist Bag
+  44244, // Elaborate Ritualist Bag
+  44245, // Elaborate Ritualist Bag
+  44246, // Elaborate Ritualist Bag
+  44247, // Embroidered Coin Purse
+  44248, // Embroidered Coin Purse
+  44249, // Embroidered Coin Purse
+  44250, // Embroidered Coin Purse
+  44251, // Embroidered Coin Purse
+  44252, // Embroidered Saddlebag
+  44253, // Embroidered Saddlebag
+  44254, // Embroidered Saddlebag
+  44255, // Embroidered Saddlebag
+  44256, // Embroidered Saddlebag
+  44257, // Embroidered Saddlebag
+  44258, // Embroidered Saddlebag
+  44259, // Embroidered Saddlebag
+  44260, // Embroidered Saddlebag
+  44261, // Embroidered Saddlebag
+  44262, // Embroidered Belt Pouch
+  44263, // Embroidered Belt Pouch
+  44264, // Embroidered Belt Pouch
+  44265, // Embroidered Belt Pouch
+  44266, // Embroidered Belt Pouch
+  44267, // Fallen Adventurer's Backpack
+  44268, // Fallen Adventurer's Backpack
+  44269, // Fallen Adventurer's Backpack
+  44270, // Fallen Adventurer's Backpack
+  44271, // Fallen Adventurer's Backpack
+  44272, // Fallen Adventurer's Backpack
+  44273, // Fallen Adventurer's Backpack
+  44274, // Fallen Adventurer's Backpack
+  44275, // Fallen Adventurer's Backpack
+  44276, // Fallen Adventurer's Backpack
+  44277, // Fallen Adventurer's Backpack
+  44278, // Fallen Adventurer's Backpack
+  44279, // Fallen Adventurer's Backpack
+  44280, // Fallen Adventurer's Backpack
+  44281, // Fallen Adventurer's Backpack
+  44282, // Fallen Adventurer's Backpack
+  44283, // Fallen Adventurer's Backpack
+  44284, // Fallen Adventurer's Backpack
+  44285, // Fallen Adventurer's Backpack
+  44286, // Fallen Adventurer's Backpack
+  44287, // Fallen Adventurer's Backpack
+  44288, // Fallen Adventurer's Backpack
+  44289, // Fallen Adventurer's Backpack
+  44290, // Fallen Adventurer's Backpack
+  44291, // Fallen Adventurer's Backpack
+  44292, // Fallen Adventurer's Backpack
+  44293, // Fallen Adventurer's Backpack
+  44294, // Fallen Adventurer's Backpack
+  44295, // Fallen Adventurer's Backpack
+  44296, // Fallen Adventurer's Backpack
+  44297, // Fallen Adventurer's Backpack
+  44298, // Fallen Adventurer's Backpack
+  44299, // Fallen Adventurer's Backpack
+  44300, // Fallen Adventurer's Backpack
+  44301, // Fallen Adventurer's Backpack
+  44302, // Fallen Adventurer's Backpack
+  44303, // Fallen Adventurer's Backpack
+  44304, // Fallen Adventurer's Backpack
+  44305, // Fallen Adventurer's Backpack
+  44306, // Fallen Adventurer's Backpack
+  44307, // Fallen Adventurer's Backpack
+  44308, // Fallen Adventurer's Backpack
+  44309, // Fallen Adventurer's Backpack
+  44310, // Fallen Adventurer's Backpack
+  44311, // Fallen Adventurer's Backpack
+  44312, // Fallen Adventurer's Backpack
+  44313, // Fallen Adventurer's Backpack
+  44314, // Fallen Adventurer's Backpack
+  44315, // Fallen Adventurer's Backpack
+  44316, // Fallen Adventurer's Backpack
+  44317, // Fallen Adventurer's Backpack
+  44318, // Fallen Adventurer's Backpack
+  44319, // Fallen Adventurer's Backpack
+  44320, // Fallen Adventurer's Backpack
+  44321, // Fallen Adventurer's Backpack
+  44322, // Fallen Adventurer's Backpack
+  44323, // Fallen Adventurer's Backpack
+  44324, // Fallen Adventurer's Backpack
+  44325, // Fallen Adventurer's Backpack
+  44326, // Fallen Adventurer's Backpack
+  44327, // Fallen Adventurer's Backpack
+  44328, // Fallen Adventurer's Backpack
+  44329, // Fallen Adventurer's Backpack
+  44330, // Fallen Adventurer's Backpack
+  44331, // Fallen Adventurer's Backpack
+  44332, // Fallen Adventurer's Backpack
+  44333, // Fallen Adventurer's Backpack
+  44334, // Fallen Adventurer's Backpack
+  44335, // Fallen Adventurer's Backpack
+  44336, // Fallen Adventurer's Backpack
+  44337, // Fallen Adventurer's Backpack
+  44338, // Fallen Adventurer's Backpack
+  44339, // Fallen Adventurer's Backpack
+  44340, // Fallen Adventurer's Backpack
+  44341, // Fallen Adventurer's Backpack
+  44342, // Fallen Adventurer's Backpack
+  44343, // Fallen Adventurer's Backpack
+  44344, // Fallen Adventurer's Backpack
+  44345, // Fallen Adventurer's Backpack
+  44346, // Fallen Adventurer's Backpack
+  44347, // Fallen Adventurer's Backpack
+  44348, // Fallen Adventurer's Backpack
+  44349, // Fallen Adventurer's Backpack
+  44350, // Fallen Adventurer's Backpack
+  44351, // Fallen Adventurer's Backpack
+  44352, // Fallen Adventurer's Backpack
+  44353, // Fallen Adventurer's Backpack
+  44354, // Fallen Adventurer's Backpack
+  44355, // Fallen Adventurer's Backpack
+  44356, // Fallen Adventurer's Backpack
+  44357, // Fallen Adventurer's Backpack
+  44358, // Fallen Adventurer's Backpack
+  44359, // Fallen Adventurer's Backpack
+  44360, // Fallen Adventurer's Backpack
+  44361, // Fallen Adventurer's Backpack
+  44362, // Fallen Adventurer's Backpack
+  44363, // Fallen Adventurer's Backpack
+  44364, // Fallen Adventurer's Backpack
+  44365, // Fallen Adventurer's Backpack
+  44366, // Fallen Adventurer's Backpack
+  44367, // Fallen Adventurer's Backpack
+  44368, // Fallen Adventurer's Backpack
+  44369, // Fallen Adventurer's Backpack
+  44370, // Fallen Adventurer's Backpack
+  44371, // Fallen Adventurer's Backpack
+  44372, // Fallen Adventurer's Backpack
+  44373, // Fallen Adventurer's Backpack
+  44374, // Fallen Adventurer's Backpack
+  44375, // Fallen Adventurer's Backpack
+  44376, // Fallen Adventurer's Backpack
+  44377, // Fallen Adventurer's Backpack
+  44378, // Fallen Adventurer's Backpack
+  44379, // Fallen Adventurer's Backpack
+  44380, // Fallen Adventurer's Backpack
+  44381, // Fallen Adventurer's Backpack
+  44382, // Fallen Adventurer's Backpack
+  44383, // Fallen Adventurer's Backpack
+  44384, // Fallen Adventurer's Backpack
+  44385, // Fallen Adventurer's Backpack
+  44386, // Fallen Adventurer's Backpack
+  44387, // Fallen Adventurer's Backpack
+  44388, // Fallen Adventurer's Backpack
+  44389, // Fallen Adventurer's Backpack
+  44390, // Fallen Adventurer's Backpack
+  44391, // Fallen Adventurer's Backpack
+  44392, // Fallen Adventurer's Backpack
+  44393, // Fallen Adventurer's Backpack
+  44394, // Fallen Adventurer's Backpack
+  44395, // Fallen Adventurer's Backpack
+  44396, // Fallen Adventurer's Backpack
+  44397, // Oversized Rucksack
+  44398, // Oversized Rucksack
+  44399, // Oversized Rucksack
+  44400, // Oversized Rucksack
+  44401, // Oversized Rucksack
+  44402, // Oversized Rucksack
+  44403, // Oversized Rucksack
+  44404, // Oversized Rucksack
+  44405, // Oversized Rucksack
+  44406, // Oversized Rucksack
+  44407, // Oversized Rucksack
+  44408, // Oversized Rucksack
+  44409, // Oversized Rucksack
+  44410, // Oversized Rucksack
+  44411, // Oversized Rucksack
+  44412, // Oversized Rucksack
+  44413, // Oversized Rucksack
+  44414, // Oversized Rucksack
+  44415, // Oversized Rucksack
+  44416, // Oversized Rucksack
+  44417, // Gilded Strongbox
+  44418, // Gilded Strongbox
+  44419, // Gilded Strongbox
+  44420, // Gilded Strongbox
+  44421, // Gilded Strongbox
+  44422, // Gilded Strongbox
+  44423, // Gilded Strongbox
+  44424, // Gilded Strongbox
+  44425, // Gilded Strongbox
+  44426, // Gilded Strongbox
+  44427, // Gilded Strongbox
+  44428, // Gilded Strongbox
+  44429, // Gilded Strongbox
+  44430, // Gilded Strongbox
+  44431, // Gilded Strongbox
+  44432, // Gilded Strongbox
+  44433, // Gilded Strongbox
+  44434, // Gilded Strongbox
+  44435, // Gilded Strongbox
+  44436, // Gilded Strongbox
+  44437, // Gilded Strongbox
+  44438, // Gilded Strongbox
+  44439, // Gilded Strongbox
+  44440, // Gilded Strongbox
+  44441, // Gilded Strongbox
+  44442, // Gilded Strongbox
+  44443, // Gilded Strongbox
+  44444, // Gilded Strongbox
+  44445, // Gilded Strongbox
+  44446, // Gilded Strongbox
+  44447, // Icy Strongbox
+  44448, // Icy Strongbox
+  44449, // Icy Strongbox
+  44450, // Icy Strongbox
+  44451, // Icy Strongbox
+  44452, // Large Candy Bag
+  44453, // Large Candy Bag
+  44454, // Large Candy Bag
+  44455, // Large Candy Bag
+  44456, // Large Candy Bag
+  44457, // Shell Pouch
+  44458, // Shell Pouch
+  44459, // Shell Pouch
+  44460, // Shell Pouch
+  44461, // Shell Pouch
+  44462, // Shell Pouch
+  44463, // Shell Pouch
+  44464, // Shell Pouch
+  44465, // Shell Pouch
+  44466, // Shell Pouch
+  44467, // Deluxe Gear Box
+  44468, // Deluxe Gear Box
+  44469, // Deluxe Gear Box
+  44470, // Deluxe Gear Box
+  44471, // Deluxe Gear Box
+  49038, // Shell Pouch
+  49039, // Heirloom Seed Pouch
+  64529, // Box of Lab Equipment
+  64530, // Thorny Seed Pouch
+  64531, // Embroidered Coin Purse
+  64532, // Icy Strongbox
+  64533, // Singed Ritualist Bag
+  66403, // Tootsie's Speed Pack
+  70941, // Feather-Lined Pouch
+  71034, // Noxious Seed Pod
+  71182, // Goop-Covered Satchel
+  71250, // Goop-Covered Satchel
+  71371, // Noxious Seed Pod
   72871, // Half-Eaten Backpack
   73442, // Half-Eaten Backpack
+  73716, // Noxious Seed Pod
   73878, // Half-Eaten Backpack
+  73958, // Feather-Lined Pouch
+  74557, // Feather-Lined Pouch
+  75235, // Feather-Lined Pouch
+  75522, // Noxious Seed Pod
+  75741, // Goop-Covered Satchel
+  75988, // Goop-Covered Satchel
+  76014, // Noxious Seed Pod
+  76042, // Goop-Covered Satchel
+  76830, // Feather-Lined Pouch
   77192, // Half-Eaten Backpack
   77199, // Half-Eaten Backpack
-  66403 // Tootsie's Speed Pack
+  79144, // Gilded Strongbox
+  79160, // Gilded Strongbox
+  79215, // Gilded Strongbox
+  79224, // Elaborate Fur Pouch
+  79272, // Gilded Strongbox
+  81781, // Sentient Seed
+  82632, // Sentient Seed
+  83110, // Flamestruck Pyxis
+  83122, // Battered Forged Components
+  83180, // Rockhewn Pyxis
+  83746, // Lost Saddlebag
+  84084, // Branded Geodermite
+  84090, // Sentient Root
+  84291, // Chewed-Up Caravan Chest
+  84305, // Palawan Phylactery
+  84381, // Phantasmic Belongings
+  84490, // Seaswept Pyxis
+  86062, // Gilded Strongbox
+  87048 // Gilded Strongbox
 ]
