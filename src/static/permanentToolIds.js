38f47fff3cadbf9394381d92c93a3b70a184d5f6
queicherius <queicherius@gmail.com>
2018-09-12 19:17:46 +0100

    Update item ids for gemstore permanent gathering tools

--- src/static/permanentToolIds.js
+++ src/static/permanentToolIds.js
@@ -1,54 +1,55 @@
 export default [
-  67032, // Fused Molten Sickle
-  67030, // Fused Molten Logging Axe
   41807, // Molten Alliance Mining Pick
-  48933, // Molten Alliance Mining Pick
-  67063, // Tireless Harvesting Minion
-  67029, // Tireless Logging Minion
-  48932, // Bone Pick
-  43527, // Bone Pick
-  67395, // Reap-r-Tron
-  67394, // Log-r-Tron
-  67396, // Mine-r-Tron
-  49308, // Thresher-Sickle 5000
+  42594, // Consortium Harvesting Sickle
   42931, // Chop-It-All Logging Axe
-  48931, // Chop-It-All Logging Axe
-  87422, // Watchwork Mining Pick
+  43527, // Bone Pick
+  44876, // Jack-in-the-Box Scythe
   47897, // Watchwork Mining Pick
-  69478, // Mad Scientist's Harvesting Tool
+  48825, // Frost Wasp Logging Tool
+  48930, // Consortium Harvesting Sickle
+  48931, // Chop-It-All Logging Axe
+  48932, // Bone Pick
+  48933, // Molten Alliance Mining Pick
+  48934, // Jack-in-the-Box Scythe
+  48955, // Dreamcleaver Logging Axe
+  49308, // Thresher-Sickle 5000
+  67029, // Tireless Logging Minion
+  67030, // Fused Molten Logging Axe
+  67032, // Fused Molten Sickle
+  67063, // Tireless Harvesting Minion
+  67394, // Log-r-Tron
+  67395, // Reap-r-Tron
+  67396, // Mine-r-Tron
   68799, // Mad Scientist's Logging Tool
   68905, // Mad Scientist's Mining Tool
-  70044, // Butterfly Harvesting Flute
+  69478, // Mad Scientist's Harvesting Tool
   69921, // Swarm Logging Flute
   69958, // Firefly Mining Flute
-  79721, // Cosmic Harvesting Tool
-  79472, // Cosmic Logging Tool
+  70044, // Butterfly Harvesting Flute
+  78724, // Focused Solar Logging Tool
   78731, // Cosmic Mining Tool
-  78996, // Unbreakable Harvesting Sickle
   78806, // Unbreakable Logging Axe
   78812, // Unbreakable Mining Pick
-  80987, // Unbound Magic Harvesting Blast
-  87396, // Unbound Magic Harvesting Blast
-  87420, // Unbound Magic Logging Pulse
-  80979, // Unbound Magic Logging Pulse
-  87443, // Unbound Magic Mining Beam
-  80977, // Unbound Magic Mining Beam
-  87701, // Volatile Magic Harvesting Tool
-  87954, // Volatile Magic Logging Tool
-  87841, // Volatile Magic Mining Tool
-  87554, // Choya Harvesting Tool
   78909, // Glitter Bomb Harvesting Tool
-  48930, // Consortium Harvesting Sickle
-  42594, // Consortium Harvesting Sickle
-  48934, // Jack-in-the-Box Scythe
-  44876, // Jack-in-the-Box Scythe
+  78996, // Unbreakable Harvesting Sickle
+  79472, // Cosmic Logging Tool
+  79721, // Cosmic Harvesting Tool
   80067, // Celestial Rooster Harvesting Tool
-  86943, // Lucky Dog Harvesting Tool
-  87397, // Lucky Dog Harvesting Tool
-  87530, // Choya Logging Tool
-  48955, // Dreamcleaver Logging Axe
-  48825, // Frost Wasp Logging Tool
-  78724, // Focused Solar Logging Tool
   80966, // Super Adventure Logging Bear
-  87425 // Shifting Sand Mining Pick
+  80977, // Unbound Magic Mining Beam
+  80979, // Unbound Magic Logging Pulse
+  80987, // Unbound Magic Harvesting Blast
+  86943, // Lucky Dog Harvesting Tool
+  87396, // Unbound Magic Harvesting Blast
+  87397, // Lucky Dog Harvesting Tool
+  87420, // Unbound Magic Logging Pulse
+  87422, // Watchwork Mining Pick
+  87425, // Shifting Sand Mining Pick
+  87443, // Unbound Magic Mining Beam
+  87530, // Choya Logging Tool
+  87554, // Choya Harvesting Tool
+  87701, // Volatile Magic Harvesting Tool
+  87841, // Volatile Magic Mining Tool
+  87954, // Volatile Magic Logging Tool
+  88355 // Choya Mining Tool
 ]
