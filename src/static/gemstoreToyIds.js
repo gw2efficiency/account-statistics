a9376458da07757a1ff93ef1794cb147fba989f8
queicherius <queicherius@gmail.com>
2018-09-12 19:13:59 +0100

    Update item ids for gemstore toys

--- src/static/gemstoreToyIds.js
+++ src/static/gemstoreToyIds.js
@@ -1,34 +1,54 @@
 export default [
-  67356, // Magic Carpet
-  49948, // Sonic Tunneling Tool
-  42664, // Sonic Tunneling Tool
-  49941, // Bloody Prince's Staff Toy
-  43092, // Bloody Prince's Staff Toy
-  39133, // Bouquet of Roses
-  49933, // Bouquet of Roses
-  43528, // Box of Chocolates
-  49935, // Enchanted Broom
-  49932, // Enchanted Broom
-  36373, // Enchanted Broom
-  43099, // Executioner Axe Toy
-  49940, // Executioner Axe Toy
-  38059, // Fancy Winter Crystal Scepter
-  64734, // Fancy Winter Crystal Scepter
-  49939, // Frying Pan
-  20262, // Frying Pan
-  43930, // Guild Kite
-  42967, // Lightning Kite
   20261, // Pirate Sword
-  49942, // Pirate Sword
+  20262, // Frying Pan
   31850, // Pirate Sword
-  68614, // Red Lantern
-  77763, // Red Lantern
-  36361, // Scepter of Thorn
-  49943, // Scepter of Thorn
-  43487, // Sun Kite
-  43076, // Wind Kite
-  67881, // Aviator's Memory Box
   36172, // Bobblehead Laboratory
+  36361, // Scepter of Thorn
+  36373, // Enchanted Broom
+  38059, // Fancy Winter Crystal Scepter
+  39133, // Bouquet of Roses
+  39134, // Riding Broom
+  42664, // Sonic Tunneling Tool
+  42967, // Lightning Kite
+  43076, // Wind Kite
+  43092, // Bloody Prince's Staff Toy
+  43099, // Executioner Axe Toy
+  43487, // Sun Kite
+  43528, // Box of Chocolates
+  43881, // Bottomless Belcher's Bluff Kit
+  43901, // Zephyr Sanctum Model
+  43930, // Guild Kite
+  49931, // Riding Broom
+  49932, // Enchanted Broom
+  49933, // Bouquet of Roses
+  49935, // Enchanted Broom
+  49939, // Frying Pan
+  49940, // Executioner Axe Toy
+  49941, // Bloody Prince's Staff Toy
+  49942, // Pirate Sword
+  49943, // Scepter of Thorn
+  49948, // Sonic Tunneling Tool
+  64734, // Fancy Winter Crystal Scepter
   66342, // Water Fight Balloon Bucket
-  80854 // Bloodstone Ascender
+  66897, // Prosperity Mine Kite
+  66898, // Crystal Shard Kite
+  66900, // Ventari Follower Kite
+  67356, // Magic Carpet
+  67881, // Aviator's Memory Box
+  68614, // Red Lantern
+  7321, // Frying Pan
+  77763, // Red Lantern
+  78771, // Electromagnetic Ascender
+  79666, // Hovering Mad Mirror
+  80854, // Bloodstone Ascender
+  84916, // Sand Shark Kite
+  84950, // Red Choya Kite
+  85100, // Blue Choya Kite
+  85173, // Zebra Skimmer Kite
+  85240, // Skimmer Kite
+  85302, // Green Choya Kite
+  85315, // Watermelon Sand Shark Kite
+  88124, // Embellished Kite
+  88131, // Mark of Peace Kite
+  88216 // Ornamental Kite
 ]
