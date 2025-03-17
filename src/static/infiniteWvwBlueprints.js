12dc1bdf2b9f5646ddbbd49968909a81401ac9be
queicherius <queicherius@gmail.com>
2025-03-17 00:27:58 +0000

    LInter

--- src/static/infiniteWvwBlueprints.js
+++ src/static/infiniteWvwBlueprints.js
@@ -5,5 +5,5 @@ export default [
   103878, // Infinite Flame Ram Blueprint
   103915, // Infinite Shield Generator Blueprint
   103911, // Infinite Siege Golem Blueprint
-  103980, // Infinite Trebuchet Blueprint
+  103980 // Infinite Trebuchet Blueprint
 ]
